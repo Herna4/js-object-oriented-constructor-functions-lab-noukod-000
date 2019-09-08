e411756a4f061fd0a16c0e1db0664824efdab8b2
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-08 23:46:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function Scooter(year, color, model){
   this.model = model
 }
 
-// const samson = new Scooter(2015, "red", "swoosh");
+const samson = new Scooter(2015, "red", "swoosh");
 
 function Driver(name, age, experience){
   this.name = name;
   this.age = age;
   this.experience = experience;
 }
-// const allison = new Driver("Allison", "16 years", "2 years");
+const allison = new Driver("Allison", "16 years", "2 years");
 
 function PickupLocation (address, city){
   this.address = address;
   this.city = city;
 }
-// const poshHotel = new PickupLocation("123 Broadway", "New York City");
+const poshHotel = new PickupLocation("123 Broadway", "New York City");
